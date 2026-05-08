109d52098d802d481fbed061e399f4cca780eb16
VasundharaDPavuluri <vasundharadp1999@gmail.com>
2026-05-08 22:11:17 +0530

    add keypair in ec2.tf

--- terraform-basic-handson/ec2.tf
+++ terraform-basic-handson/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "terraform_ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t2.micro"
+  key_name = "terraform-key"
 
   tags = {
     Name = "Terraform-EC2"
